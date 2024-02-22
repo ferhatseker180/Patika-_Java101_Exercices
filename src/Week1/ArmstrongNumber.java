191d4cf39460fd84ce56ec776b64c55ddbd5141d
FERHAT <73959099+ferhatseker180@users.noreply.github.com>
2024-02-23 00:48:58 +0300

    The user enters his information and the system confirms it, if the information is incorrect, it shows the remaining number of rights and sends it back to the password page. If the information is correct, it is sent to the home page to choose the action to be taken and it selects the actions to be taken there.

---
+++ src/Week1/ArmstrongNumber.java
@@ -0,0 +1,21 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+
+        // found sum of numbers in the step. This is shortcut.
+        /*
+      while (number != 0){
+          stepCount = number % 10;
+
+          sum += stepCount;
+          number /= 10;
+      }
+
+         */
+
+
+    }
+}
