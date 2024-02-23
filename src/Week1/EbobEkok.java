5d80232bd2c2683887626ac728f74fe9663e7db1
FERHAT <73959099+ferhatseker180@users.noreply.github.com>
2024-02-23 18:33:58 +0300

    Calculate Ebob-Ekok

--- src/Week1/EbobEkok.java
+++ src/Week1/EbobEkok.java
@@ -6,7 +6,7 @@ public class EbobEkok {
     public static void main(String[] args) {
 
         // define variables
-        int number1, number2, ebob = 1, ekok;
+        int number1, number2, ebob = 1, ekok, i = 1;
 
         // import scanner
         Scanner input = new Scanner(System.in);
@@ -15,7 +15,34 @@ public class EbobEkok {
         System.out.print("Enter The Second Number: ");
         number2 = input.nextInt();
 
-        // compare number1 and number2
+        // compare number1 and number2 with if and while loop
+
+        if (number1 < number2) {
+            while (i <= number1) {
+                if (number1 % i == 0 && number2 % i == 0) {
+                    ebob = i;
+                }
+                i++;
+            }
+        }
+
+        if (number2 < number1) {
+            while (i <= number2) {
+                if (number1 % i == 0 && number2 % i == 0) {
+                    ebob = i;
+                }
+                i++;
+            }
+        }
+
+        // Ekok formula
+        ekok = (number1 * number2) / ebob;
+
+        System.out.println("Ebob: " + ebob);
+        System.out.println("Ekok: " + ekok);
+
+        /*
+                // compare number1 and number2 with if and while loop
 
         if (number1 < number2) {
             for (int i = 1; i <= number1; i++) {
@@ -34,11 +61,7 @@ public class EbobEkok {
             }
         }
 
-        // Ekok formula
-        ekok = (number1 * number2) / ebob;
-
-        System.out.println("Ebob: " + ebob);
-        System.out.println("Ekok: " + ekok);
+         */
 
 
     }
